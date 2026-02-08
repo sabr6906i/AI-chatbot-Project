56735c6f2764fb3ff0e4e5f12074a0f7eb926cf5
sabr6906i <sarbjeetsinghpal2006@gmail.com>
2026-02-08 21:06:58 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,17 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompts.py
+touch research/__init__.py
+touch .env
+touch setup.py
+touch README.md
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
+
+
