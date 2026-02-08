288ac9f36d63a6752f1aa6c5e37f9426069057b6
sabr6906i <sarbjeetsinghpal2006@gmail.com>
2026-02-08 22:14:07 +0530

    new updates

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ai_chatbot_project",
+    version="0.1.0",
+    author="Sarbjeet Singh Pal",
+    author_email="sabri6906@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
